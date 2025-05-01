1da992ff54b7100b7477bba25dbfc3b873d4cbb2
ColabFinder <colabfinderhq@gmail.com>
2025-04-30 20:06:13 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 // supabaseClient.js
-const supabaseUrl = 'https://eqpmbcbaqgdmrhwmvlya.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcG1iY2JhcWdkbXJod212bHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4NDg4ODQsImV4cCI6MjA2MDQyNDg4NH0.V3SwBCiBkGO_YxTKnE7jbdFthmXAJNbiEVcjsLUYCaM';
 
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-window.supabase = supabase; // Make it available globally
+const SUPABASE_URL = 'https://eqpmbcbaqgdmrhwmvlya.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcG1iY2JhcWdkbXJod212bHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4NDg4ODQsImV4cCI6MjA2MDQyNDg4NH0.V3SwBCiBkGO_YxTKnE7jbdFthmXAJNbiEVcjsLUYCaM';
+
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
